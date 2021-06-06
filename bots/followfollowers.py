b29f3e88261e6b69cc27e39960d967e45be8276f
cristiansuzuki <cristiansuzuki5@hotmail.com>
2021-06-06 00:33:51 -0300

    primeiro commit

---
+++ bots/followfollowers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# tweepy-bots/bots/followfollowers.py
+
+import tweepy
+import logging
+from config import create_api
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def follow_followers(api):
+    logger.info("Retrieving and following followers")
+    for follower in tweepy.Cursor(api.followers).items():
+        if not follower.following:
+            logger.info(f"Following {follower.name}")
+            follower.follow()
+
+def main():
+    api = create_api()
+    while True:
+        follow_followers(api)
+        logger.info("Waiting...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
