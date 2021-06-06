71da6ac566d117fa99b31c93a0da402512a2701a
cristiansuzuki <cristiansuzuki5@hotmail.com>
2021-06-06 03:48:52 -0300

    daily tweet created

--- bots/favretweet.py
+++ bots/favretweet.py
@@ -4,7 +4,7 @@
 import tweepy
 import logging
 from config import create_api
-import json
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -26,21 +26,24 @@ class FavRetweetListener(tweepy.StreamListener):
                 tweet.favorite()
             except Exception as e:
                 logger.error("Error on fav", exc_info=True)
-        if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
-            try:
-                tweet.retweet()
-            except Exception as e:
-                logger.error("Error on fav and retweet", exc_info=True)
+        # if not tweet.retweeted:
+        #     # Retweet, since we have not retweeted it yet
+        #     try:
+        #         tweet.retweet()
+        #     except Exception as e:
+        #         logger.error("Error on fav and retweet", exc_info=True)
 
     def on_error(self, status):
         logger.error(status)
 
 def main(keywords):
     api = create_api()
-    tweets_listener = FavRetweetListener(api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["en"])
-
+    while True:
+        tweets_listener = FavRetweetListener(api)
+        stream = tweepy.Stream(api.auth, tweets_listener)
+        stream.filter(track=keywords, languages=["en"])
+        logger.info("Waiting...")
+        time.sleep(60)
+        
 if __name__ == "__main__":
-    main(["Durant"])
+    main(["#python"])
