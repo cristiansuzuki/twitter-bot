56805767e4de2472260d75d28ed48d07c90094f5
cristiansuzuki <cristiansuzuki5@hotmail.com>
2022-04-12 15:59:47 -0300

    assist leaders function working

--- bot.py
+++ bot.py
@@ -60,21 +60,23 @@ def follow_followers(api):
             pass
         
 def daily_tweet(api, last_tweeted):
-    # Bate na API para retornar os lideres em pontos totais
+    # Lideres totais em pontos na temporada
     data_pontos = endpoints.leagueleaders.LeagueLeaders(season = SeasonAll.current_season) 
     df_pontos = data_pontos.league_leaders.get_data_frame()
-    
     pontuadores_totais = 'NBA - Top 5 da temporada: Pontos totais ' + '\n' + '\n' + str(df_pontos.PLAYER[0]) + ' - ' + str(df_pontos.PTS[0]) + ' pontos' + '\n' + str(df_pontos.PLAYER[1]) + ' - ' + str(df_pontos.PTS[1]) + ' pontos' + '\n' + str(df_pontos.PLAYER[2]) + ' - ' + str(df_pontos.PTS[2]) + ' pontos' + '\n' + str(df_pontos.PLAYER[3]) + ' - ' + str(df_pontos.PTS[3]) + ' pontos' + '\n' + str(df_pontos.PLAYER[4]) + ' - ' + str(df_pontos.PTS[4]) + ' pontos'
 
-    # Bate na API para retornar os lideres em rebotes
+    # Lideres em rebotes totais na temporada
     data_rebotes = endpoints.leagueleaders.LeagueLeaders(season = SeasonAll.current_season, stat_category_abbreviation = 'REB') 
     df_rebotes = data_rebotes.league_leaders.get_data_frame()
-    
-    rebotes_totais = 'NBA - Top 5 da temporada: Rebotes totais' + '\n' + '\n' + str(df_rebotes.PLAYER[0]) + ' - ' + str(df_rebotes.PTS[0]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[1]) + ' - ' + str(df_rebotes.PTS[1]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[2]) + ' - ' + str(df_rebotes.PTS[2]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[3]) + ' - ' + str(df_rebotes.PTS[3]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[4]) + ' - ' + str(df_rebotes.PTS[4]) + ' rebotes'
+    rebotes_totais = 'NBA - Top 5 da temporada: Rebotes totais' + '\n' + '\n' + str(df_rebotes.PLAYER[0]) + ' - ' + str(df_rebotes.REB[0]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[1]) + ' - ' + str(df_rebotes.REB[1]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[2]) + ' - ' + str(df_rebotes.REB[2]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[3]) + ' - ' + str(df_rebotes.REB[3]) + ' rebotes' + '\n' + str(df_rebotes.PLAYER[4]) + ' - ' + str(df_rebotes.REB[4]) + ' rebotes'
 
+    # Lideres em assistencias totais da temporada
+    data_assistencias = endpoints.leagueleaders.LeagueLeaders(season = SeasonAll.current_season, stat_category_abbreviation = 'AST')
+    df_assistencias = data_assistencias.league_leaders.get_data_frame()
+    assistencias_totais = 'NBA - Top 5 da temporada: Assistencias totais' + '\n' + '\n' + str(df_assistencias.PLAYER[0]) + ' - ' + str(df_assistencias.AST[0]) + ' assistencias' + '\n' + str(df_assistencias.PLAYER[1]) + ' - ' + str(df_assistencias.AST[1]) + ' assistencias' + '\n' + str(df_assistencias.PLAYER[2]) + ' - ' + str(df_assistencias.AST[2]) + ' assistencias' + '\n' + str(df_assistencias.PLAYER[3]) + ' - ' + str(df_assistencias.AST[3]) + ' assistencias' + '\n' + str(df_assistencias.PLAYER[4]) + ' - ' + str(df_assistencias.AST[4]) + ' assistencias'
 
     if last_tweeted < datetime.now()-timedelta(hours=12):
-        api.update_status(pontuadores_totais)
+        api.update_status(rebotes_totais)
 
         logger.info('Stats publicado com sucesso !')        
         return datetime.now()
